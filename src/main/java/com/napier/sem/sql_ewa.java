9a78e033183d966f61429a45143fe483766934a0
eolton <157489372+eolton@users.noreply.github.com>
2024-03-05 11:47:35 +0000

    SQL Query - REGION - Fetch population values for all the COUNTRIES in SELECTED REGION from the database.

--- src/main/java/com/napier/sem/sql_ewa.java
+++ src/main/java/com/napier/sem/sql_ewa.java
@@ -15,4 +15,11 @@ public class sql_ewa {
     "ON country.capital = city.id
     "WHERE country.continent = "South America"
     "ORDER BY country.Population DESC;
+
+    //REGION - Fetch population values for all the COUNTRIES in SELECTED REGION from the database.SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name AS Capital
+    "FROM world.country
+    "JOIN world.city
+    "ON country.capital = city.id
+    "WHERE country.region = "Caribbean"
+    "ORDER BY country.Population DESC;
 }
