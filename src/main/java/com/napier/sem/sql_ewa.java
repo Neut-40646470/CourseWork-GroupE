96cd9bc4cf04ddfbe7a02313fb5e49597e97cb24
eolton <157489372+eolton@users.noreply.github.com>
2024-03-05 12:41:26 +0000

    SQL Query - CITIES - Fetch ALL population from SELECTED COUNTRY

--- src/main/java/com/napier/sem/sql_ewa.java
+++ src/main/java/com/napier/sem/sql_ewa.java
@@ -43,4 +43,12 @@ public class sql_ewa {
     "FROM world.city
     "JOIN world.country ON city.CountryCode = country.Code
     "ORDER BY city.Population DESC;
+
+    //CITIES - Fetch ALL population from SELECTED COUNTRY
+    "SELECT city.Name, country.Name AS Country, city.District, city.Population AS Population
+    "FROM city
+    "JOIN country
+    "ON city.CountryCode = country.Code
+    "WHERE country.Name = "Poland"
+    "ORDER BY Population DESC;
 }
