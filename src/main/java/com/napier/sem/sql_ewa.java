53dee7a8f167fb15ac8d7bb2bceb4d22ecf96354
eolton <157489372+eolton@users.noreply.github.com>
2024-03-05 12:22:42 +0000

    SQL Query - REGION - Fetch population for all CITIES from SELECTED REGION

--- src/main/java/com/napier/sem/sql_ewa.java
+++ src/main/java/com/napier/sem/sql_ewa.java
@@ -30,5 +30,11 @@ public class sql_ewa {
     "WHERE city.District = "California"
     "ORDER BY city.Population DESC;
 
-    
+    //REGION - Fetch population for all CITIES from SELECTED REGION
+    "SELECT city.Name, country.Name AS Country, city.District, city.Population
+    "FROM city
+    "JOIN country
+    "ON city.CountryCode = country.Code
+    "WHERE country.Region = "Southern Europe"
+    "ORDER BY Population DESC;
 }
