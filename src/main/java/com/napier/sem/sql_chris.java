4ae0063f65c8481eddd90cd57aab22b552cc812b
Christopher Allan <40674826@live.napier.ac.uk>
2024-03-05 11:20:07 +0000

    SELECT STATEMENTS WIP

--- src/main/java/com/napier/sem/sql_chris.java
+++ src/main/java/com/napier/sem/sql_chris.java
@@ -6,4 +6,34 @@ public class sql_chris {
     "FROM world.city;"
 
 
+  //WORLD - Fetch population of CAPITAL CITIES
+ //(Outputs country joined with its capital city followed by population in largest to smallest/DESC order)
+    "SELECT co.NAME AS Country,""
+    "p.NAME AS CapitalCity,""
+    "p.Population AS Population""
+    "FROM world.city p""
+    "JOIN world.country co ON p.ID = co.Capital""
+    "ORDER BY p.Population DESC;""
+
+
+     //COUNTRY - Fetch population of CITIES in SELECTED COUNTRY
+    //"(Below will pull the population of cities in a selected country which is placed in 'COUNTRY NAME HERE', have added ORDER BY in the case of Top (N) populated cities task)
+   "SELECT c.Name AS Cities, c.Population AS Population"
+    "FROM world.city c"
+    "JOIN world.country co ON c.CountryCode = co.Code"
+    "WHERE co.Name = 'COUNTRY NAME HERE'"
+    "ORDER BY c.Population DESC"
+
+    //CONTINENT - Fetch population of ALL CITIES from SELECTED CONTINENT
+    //(Below will pull the population of all cities in a selected continent which is placed in 'CONTINENT HERE', have added ORDER BY in the case of Top (N) populated cities task.
+
+    "SELECT c.Name AS Cities,"
+    "c.Population AS Population"
+    "FROM world.city c"
+    "JOIN world.country co ON c.CountryCode = co.Code"
+    "WHERE co.Continent = 'CONTINENT HERE'"
+    "ORDER BY c.Population DESC;"
+
+
+
 }
