50b8dbd1e90ef395f75eb0b7d2be539360d82cd0
Christopher Allan <40674826@live.napier.ac.uk>
2024-03-05 12:17:00 +0000

    SELECT STATEMENTS WIP (removed main)

--- src/main/java/com/napier/sem/sql_chris.java
+++ src/main/java/com/napier/sem/sql_chris.java
@@ -2,8 +2,11 @@ package com.napier.sem;
 
 public class sql_chris {
     //Output for WORLD - Fetch population of CITIES
-    "SELECT Name, Population"
-    "FROM world.city;"
+    //Amended code below to show all required columns
+
+    "SELECT city.Name AS Name, country.Name AS Country, city.District, city.Population"
+    "FROM world.city""
+    "JOIN world.country ON city.CountryCode = country.Code;""
 
 
   //WORLD - Fetch population of CAPITAL CITIES (Amended name of column to fit report requirement)
@@ -18,21 +21,24 @@ public class sql_chris {
 
      //COUNTRY - Fetch population of CITIES in SELECTED COUNTRY
     //"(Below will pull the population of cities in a selected country which is placed in 'COUNTRY NAME HERE', have added ORDER BY in the case of Top (N) populated cities task)
-   "SELECT c.Name AS Cities, c.Population AS Population"
-    "FROM world.city c"
+     //amended code to show required columns for a city report, (country is the choice within the code and city is the output)
+
+    "SELECT c.Name AS Name, co.Name AS Country, c.District, c.Population"
+    "FROM world.city c""
     "JOIN world.country co ON c.CountryCode = co.Code"
-    "WHERE co.Name = 'COUNTRY NAME HERE'"
-    "ORDER BY c.Population DESC"
+    "WHERE co.Name = 'Country Name Here'""
+    "ORDER BY c.Population DESC;"
 
     //CONTINENT - Fetch population of ALL CITIES from SELECTED CONTINENT
     //(Below will pull the population of all cities in a selected continent which is placed in 'CONTINENT HERE', have added ORDER BY in the case of Top (N) populated cities task.
+    //amended code to show required columns for a city report, (continent is the choice within the code and city is the output)
 
-    "SELECT c.Name AS Cities,"
-    "c.Population AS Population"
-    "FROM world.city c"
-    "JOIN world.country co ON c.CountryCode = co.Code"
-    "WHERE co.Continent = 'CONTINENT HERE'"
-    "ORDER BY c.Population DESC;"
+    "SELECT c.Name AS Name,co.Name AS Country, c.District AS District, c.Population AS Population
+    "FROM world.city c
+    "JOIN world.country co ON c.CountryCode = co.Code
+   "WHERE co.Continent = 'Continent Here'
+    "ORDER BY c.Population DESC;
+
 
 
 
