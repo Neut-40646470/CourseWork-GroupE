77f1a1c769b20b828f52385cb347c04cbdaa8de3
steven woodhead <157484405+40582347@users.noreply.github.com>
2024-03-11 11:53:57 +0000

    ALL COUNTRIES

---
+++ src/main/java/com/napier/sem/allCountries.sql
@@ -0,0 +1,6 @@
+--//COUNTRIES - Fetch population for all the COUNTRIES in the WORLD from the database.
+    SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name AS Capital
+    FROM world.country
+    JOIN world.city
+    ON country.capital = city.id
+    ORDER BY country.Population DESC;
