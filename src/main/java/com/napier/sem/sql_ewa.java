35cb54047e13a5735cee329686dbc858872595e0
Ewa Olton <40679395@live.napier.ac.uk>
2024-03-21 20:47:01 +0000

    ALL CITIES from SELECTED COUNTRY

--- src/main/java/com/napier/sem/sql_ewa.java
+++ src/main/java/com/napier/sem/sql_ewa.java
@@ -30,7 +30,7 @@
 //    "WHERE city.District = "California"
 //    "ORDER BY city.Population DESC;
 
-    //REGION - Fetch population for all CITIES from SELECTED REGION
+    //REGION - Fetch population for ALL CITIES from SELECTED REGION.sql
 //    "SELECT city.Name, country.Name AS Country, city.District, city.Population
 //    "FROM city
 //    "JOIN country
@@ -38,7 +38,7 @@
 //    "WHERE country.Region = "Southern Europe"
 //    "ORDER BY Population DESC;
 
-    //CITIES - Fetch ALL population from WORLD
+    //CITIES - Fetch ALL CITIES from WORLD.sql
 //    "SELECT city.Name, country.Name AS Country, city.District, city.Population AS Population
 //    "FROM world.city
 //    "JOIN world.country ON city.CountryCode = country.Code
