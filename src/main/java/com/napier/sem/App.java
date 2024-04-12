aef1d143b56a7ce9dcb0ce42dd324918ed2ceb71
steven woodhead <157484405+40582347@users.noreply.github.com>
2024-04-13 00:10:49 +0100

    retry everything 1

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,8 @@
 package com.napier.sem;
 
-import java.util.Scanner;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello Word");
     }
