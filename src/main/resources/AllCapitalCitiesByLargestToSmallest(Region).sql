a14f606f59e3d8551532ce08e394373eb8d1262c
Ewa Olton <40679395@live.napier.ac.uk>
2024-04-13 08:46:51 +0100

    Updated AllCapitalCities By region SQL

--- src/main/resources/AllCapitalCitiesByLargestToSmallest(Region).sql
+++ src/main/resources/AllCapitalCitiesByLargestToSmallest(Region).sql
@@ -1,5 +1,5 @@
 SELECT c.Name AS Name, co.Name AS Country, c.Population AS Population
 FROM world.city c
 JOIN world.country co ON c.CountryCode = co.Code
-WHERE co.Region = 'INSERT REGION' AND c.ID = co.Capital
+WHERE co.Region = 'Nordic Countries' AND c.ID = co.Capital
 ORDER BY c.Population DESC;
