88fde0a026f106be862116cdc73d218b5373289f
Neut-40646470 <158163064+Neut-40646470@users.noreply.github.com>
2024-04-16 09:08:33 +0100

    Update DatabaseConnector.java
    added comments.

--- src/main/java/com/napier/sem/DatabaseConnector.java
+++ src/main/java/com/napier/sem/DatabaseConnector.java
@@ -5,11 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
-    private static Connection connection = null;
+    private static Connection connection = null;  // Holds a singleton instance of the connection
 
+    // Synchronized method ensuring thread saftey
     public static synchronized Connection connect() {
+        // Check if a connection already exists and return it if so
         if (connection != null) return connection;
 
+        // Attempt to load the JDBC driver
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -17,24 +20,26 @@ public class DatabaseConnector {
             return null;
         }
 
-        int retries = 10;
+        int retries = 30; // Number of retries for connecting to the database
         for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
             try {
                 Thread.sleep(1000);
+                // Attempt a connection to the database
                 connection = DriverManager.getConnection(
                         "jdbc:mysql://mysql:3306/world?useSSL=false&allowPublicKeyRetrieval=true",
                         "root", "123");
                 System.out.println("Successfully connected");
-                break;
+                break; // Exit loop if connection is successful
             } catch (SQLException | InterruptedException e) {
                 System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(e.getMessage());
+                // Properly handle thread interruption
                 if (e instanceof InterruptedException) {
                     Thread.currentThread().interrupt();
                 }
             }
         }
-        return connection;
+        return connection; // Return the established connection or null if failed
     }
 }
